de3b9449b56c6795ee58db128bf397970770962a
Erik Sundell <erik@sundellopensource.se>
2021-07-01 02:41:01 +0200

    Struggle to get nfs home directories and lifecycle hooks

--- chart/files/etc/singleuser/k8s-lifecycle-hook-post-start.sh
+++ chart/files/etc/singleuser/k8s-lifecycle-hook-post-start.sh
@@ -8,17 +8,17 @@
 # way, we avoid reliance on GitHub and can manage a large amount of users at
 # thte same time trying to access GitHub from a limited IP range that could be
 # throttled. We fallback to using GitHub though.
-gitpuller /nh/curriculum master ~/curriculum || gitpuller https://github.com/neurohackademy/nh2021-curriculum.git master ~/curriculum || true
+gitpuller /nh/curriculum main ~/curriculum || gitpuller https://github.com/neurohackademy/nh2021-curriculum.git master ~/curriculum || true
 
 # Provide a symbolic link to the /nh/data folder in the home directory.
 [ -d ~/data ] || ln -s /nh/data/ ~/data || true
 
-# Provide a symbolic link to the /nh/data/nilearn_data folder in the home
-# directory. Requested by Elizabeth DuPre and Pierre Bellec.
-[ -d ~/nilearn_data ] || ln -s /nh/data/nilearn_data ~/nilearn_data || true
+# # Provide a symbolic link to the /nh/data/nilearn_data folder in the home
+# # directory. Requested by Elizabeth DuPre and Pierre Bellec.
+# [ -d ~/nilearn_data ] || ln -s /nh/data/nilearn_data ~/nilearn_data || true
 
-# Provide a symbolic link to /nh/data/misc/.npythyrc. Requested by Noah Benson.
-[ -f ~/.npythyrc ] || ln -s /nh/data/misc/.npythyrc ~/.npythyrc || true
+# # Provide a symbolic link to /nh/data/misc/.npythyrc. Requested by Noah Benson.
+# [ -f ~/.npythyrc ] || ln -s /nh/data/misc/.npythyrc ~/.npythyrc || true
 
 # Remove empty lost+found directories
 rmdir ~/lost+found/ || true
--- chart/files/usr/local/etc/jupyter/jupyter_notebook_config.py
+++ chart/files/usr/local/etc/jupyter/jupyter_notebook_config.py
@@ -1,8 +1,10 @@
+# This file has been used to provide some launcher entries for various content
+# in the shared data folder in the past.
 c.ServerProxy.servers = {
-  'we-nipreps-esteban': {
-    'command': ['python3', '-m', 'http.server', '--directory', '/home/jovyan/data/openneuro/ds000114/derivatives/fmriprep-20.2.0rc0/fmriprep/', '{port}'],
-  },
-  'we-nilearn-dupre': {
-    'command': ['python3', '-m', 'http.server', '--directory', '/nh/curriculum/we-nilearn-dupre/book', '{port}'],
-  }
+  # 'we-nipreps-esteban': {
+  #   'command': ['python3', '-m', 'http.server', '--directory', '/home/jovyan/data/openneuro/ds000114/derivatives/fmriprep-20.2.0rc0/fmriprep/', '{port}'],
+  # },
+  # 'we-nilearn-dupre': {
+  #   'command': ['python3', '-m', 'http.server', '--directory', '/nh/curriculum/we-nilearn-dupre/book', '{port}'],
+  # }
 }
